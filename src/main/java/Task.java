35b0af1069270e5b4bb1521659d07343a38aff24
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-13 21:01:24 -0700

    add and test static task list

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,18 @@
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task {
   private String mDescription;
   private boolean mCompleted;
   private LocalDateTime mCreatedAt;
+  private static List<Task> instances = new ArrayList<Task>();
 
   public Task(String description) {
       mDescription = description;
       mCompleted = false;
       mCreatedAt = LocalDateTime.now();
+      instances.add(this);
   }
 
   public String getDescription() {
@@ -21,5 +25,9 @@ public class Task {
 
   public LocalDateTime getCreatedAt() {
     return mCreatedAt;
+  }
+
+  public static List<Task> all() {
+    return instances;
   }
 }
