d1264340263cd6d297305e46533e50ffab64ac31
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-13 20:55:11 -0700

    add and test task properties

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
   @Test
@@ -12,5 +13,17 @@ public class TaskTest {
   public void Task_instantiatesWithDescription_String() {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
   }
 }
