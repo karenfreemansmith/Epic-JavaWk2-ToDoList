9fc8ef343ff6e492f54e54641238c4dee594bb88
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-19 19:35:40 -0700

    add Task test for datebase changes

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -17,6 +17,28 @@ public class TaskTest {
   }
 
   @Test
+  public void equals_returnsTrueIfDescriptionsAreSame() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Mow the lawn");
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAreSame() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    assertTrue(Task.all().get(0).equals(myTask));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    Task savedTask = Task.all().get(0);
+    assertEquals(myTask.getId(), savedTask.getId());
+  }
+
+  @Test
   public void Task_instantiatesWithDescription_String() {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
@@ -37,9 +59,11 @@ public class TaskTest {
   @Test
   public void all_returnsAllInstancesOfTask_true() {
     Task firstTask = new Task("Mow the lawn");
+    firstTask.save();
     Task secondTask = new Task("Buy groceries");
-    assertEquals(true, Task.all().contains(firstTask));
-    assertEquals(true, Task.all().contains(secondTask));
+    secondTask.save();
+    assertEquals(true, Task.all().get(0).equals(firstTask));
+    assertEquals(true, Task.all().get(1).equals(secondTask));
   }
 
   @Test
@@ -50,16 +74,18 @@ public class TaskTest {
   }
 
   @Test
-  public void getId_tasksInstantiateWithAnID_1() {
-    Task.clear();
+  public void getId_tasksInstantiateWithAnID() {
     Task myTask = new Task("Mow the lawn");
-    assertEquals(1,myTask.getId());
+    myTask.save();
+    assertTrue(myTask.getId()>0);
   }
 
   @Test
   public void find_returnsTaskWithSameId_secondTask() {
     Task firstTask = new Task("Mow the lawn");
+    firstTask.save();
     Task secondTask = new Task("Buy groceries");
+    secondTask.save();
     assertEquals(Task.find(secondTask.getId()), secondTask);
   }
 
