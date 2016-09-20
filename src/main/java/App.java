bbc31d8549a724e44763d8f2a168c3d2a0d31524
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-19 20:55:57 -0700

    add front end code for updating tasks

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -56,6 +56,31 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/categories/:category_id/tasks/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("category", category);
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(mode, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/categories/:category_id/tasks/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params("id")));
+      String description = request.queryParams("description");
+      Category category = Category.find(Integer.parseInt(request.params(":category_id")));
+      task.update(description);
+      String url = String.format("/categories/%d/tasks/%d", category.getId(), task.getId());
+      response.redirect(url);
+      model.put("category", category);
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(mode, layout);
+    }, new VelocityTemplateEngine());
+
+  
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
